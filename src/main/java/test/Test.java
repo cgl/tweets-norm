d8ea3f26cf5aca8bf17366b45c2d286e6f27921a
Çağıl Uluşahin <cagilulusahin@gmail.com>
2014-06-03 17:01:29 +0300

    test for mongo

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,26 @@
+package test;
+
+import graph.Mongo;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by cagil on 27/05/14.
+ */
+public class Test {
+
+
+    public static void main(String[] args) throws UnknownHostException {
+        mongoTest();
+    }
+
+    public static void mongoTest() {
+        try {
+            Mongo mongo = new Mongo();
+            System.out.print(mongo.dic.count());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
